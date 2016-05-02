547e061711803a55ce384d43603bb8e544c2d0fc
Richard van Heest <richard.van.heest@dans.knaw.nl>
2016-05-02 14:06:10 +0200

    logging and loading of properties

--- src/main/resources/archetype-resources/src/main/scala/CommandLineOptions.scala
+++ src/main/resources/archetype-resources/src/main/scala/CommandLineOptions.scala
@@ -6,9 +6,16 @@ package ${package}
 import java.io.{File, PrintWriter}
 import java.net.URL
 
+import nl.knaw.dans.easy.license.CommandLineOptions._
+import nl.knaw.dans.easy.license.Parameters
+import org.apache.commons.configuration.PropertiesConfiguration
 import org.rogach.scallop.ScallopConf
+import org.slf4j.LoggerFactory
 
 class CommandLineOptions(args: Array[String]) extends ScallopConf(args) {
+
+  import CommandLineOptions.log
+
   printedName = "${artifactId}"
   val _________ = " " * printedName.length
 
@@ -29,9 +36,22 @@ class CommandLineOptions(args: Array[String]) extends ScallopConf(args) {
 
 object CommandLineOptions {
 
+  val log = LoggerFactory.getLogger(getClass)
+
   def parse(args: Array[String]): Parameters = {
     val opts = new CommandLineOptions(args)
+    log.debug("Loading application properties ...")
+    val props = {
+      val homeDir = new File(System.getProperty("app.home"))
+      new PropertiesConfiguration(new File(homeDir, "cfg/application.properties"))
+    }
+    log.debug("Parsing command line ...")
+
     // Fill Parameters with values from command line
-    Parameters()
+    val params = Parameters()
+
+    log.debug(s"Using the following settings: ${symbol_dollar}params")
+
+    params
   }
 }

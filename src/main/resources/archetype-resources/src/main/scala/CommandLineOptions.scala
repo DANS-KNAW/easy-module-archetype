39aabcfbec7ef40ad4ec2471990808b509f275a2
Joke Pol <joke.pol@dans.knaw.nl>
2017-01-09 09:08:47 +0100

    added readme tests

--- src/main/resources/archetype-resources/src/main/scala/CommandLineOptions.scala
+++ src/main/resources/archetype-resources/src/main/scala/CommandLineOptions.scala
@@ -1,13 +1,27 @@
+/**
+ * Copyright (C) 2017 DANS - Data Archiving and Networked Services (info@dans.knaw.nl)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
 package ${package}
 
-import java.io.{File, PrintWriter}
-import java.net.URL
+import java.io.File
 
 import ${package}.CommandLineOptions._
-import ${package}.Parameters
 import org.apache.commons.configuration.PropertiesConfiguration
 import org.rogach.scallop.ScallopConf
 import org.slf4j.LoggerFactory
@@ -20,16 +34,18 @@ class CommandLineOptions(args: Array[String]) extends ScallopConf(args) {
   editBuilder(_.setHelpWidth(110))
 
   printedName = "${artifactId}"
-  val _________ = " " * printedName.length
+  val description = s"""${description}"""
+  val synopsis = s"""${symbol_dollar}printedName \\
+           |      <synopsis of command line parameters> \\
+           |      <...possibly continued again, or all joined on one line>""".stripMargin
 
   version(s"${symbol_dollar}printedName v${symbol_dollar}{Version()}")
   banner(s"""
-           |<Replace with one sentence describing the main task of this module>
+           |  ${symbol_dollar}description
            |
            |Usage:
            |
-           |${symbol_dollar}printedName <synopsis of command line parameters>
-           |${symbol_dollar}{_________} <...possibly continued here>
+           |  ${symbol_dollar}synopsis
            |
            |Options:
            |""".stripMargin)

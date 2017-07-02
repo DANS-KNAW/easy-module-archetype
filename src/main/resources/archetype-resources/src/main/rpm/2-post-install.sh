73dcca03788af278e1ee48080148fa93dd332ba5
Jan van Mansum <jan.van.mansum@dans.knaw.nl>
2017-07-02 19:09:36 +0200

    WIP

--- src/main/resources/archetype-resources/src/main/rpm/2-post-install.sh
+++ src/main/resources/archetype-resources/src/main/rpm/2-post-install.sh
@@ -3,7 +3,6 @@
 #set( $symbol_escape = '\' )
 #!/usr/bin/env bash
 
-${symbol_pound}include <log.sh>
 ${symbol_pound}include <service.sh>
 
 NUMBER_OF_INSTALLATIONS=${symbol_dollar}1
@@ -11,8 +10,8 @@ MODULE_NAME=${artifactId}
 INSTALL_DIR=/opt/dans.knaw.nl/${symbol_dollar}MODULE_NAME
 PHASE="POST-INSTALL"
 
-log_script_start ${symbol_dollar}PHASE ${symbol_dollar}NUMBER_OF_INSTALLATIONS
+echo "${symbol_dollar}PHASE: START (Number of current installations: ${symbol_dollar}NUMBER_OF_INSTALLATIONS)"
 service_install_initd_service_script "${symbol_dollar}INSTALL_DIR/bin/${symbol_dollar}MODULE_NAME-initd.sh" ${symbol_dollar}MODULE_NAME
 service_install_systemd_unit "${symbol_dollar}INSTALL_DIR/bin/${symbol_dollar}MODULE_NAME.service"
 service_create_log_directory ${symbol_dollar}MODULE_NAME
-log_script_done ${symbol_dollar}PHASE
+echo "${symbol_dollar}PHASE: DONE"
--- src/main/resources/archetype-resources/src/main/rpm/4-post-remove.sh
+++ src/main/resources/archetype-resources/src/main/rpm/4-post-remove.sh
@@ -3,7 +3,6 @@
 #set( $symbol_escape = '\' )
 #!/usr/bin/env bash
 
-${symbol_pound}include <log.sh>
 ${symbol_pound}include <service.sh>
 
 NUMBER_OF_INSTALLATIONS=${symbol_dollar}1
@@ -11,7 +10,7 @@ MODULE_NAME=${artifactId}
 INSTALL_DIR=/opt/dans.knaw.nl/${symbol_dollar}MODULE_NAME
 PHASE="POST-REMOVE"
 
-log_script_start ${symbol_dollar}PHASE ${symbol_dollar}NUMBER_OF_INSTALLATIONS
+echo "${symbol_dollar}PHASE: START (Number of current installations: ${symbol_dollar}NUMBER_OF_INSTALLATIONS)"
 service_remove_initd_service_script ${symbol_dollar}MODULE_NAME ${symbol_dollar}NUMBER_OF_INSTALLATIONS
 service_remove_systemd_unit ${symbol_dollar}MODULE_NAME ${symbol_dollar}NUMBER_OF_INSTALLATIONS
-log_script_done ${symbol_dollar}PHASE
+echo "${symbol_dollar}PHASE: DONE"

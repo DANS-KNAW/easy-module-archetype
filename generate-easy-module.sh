7854921b970e23314a77f2c1c36cbb7a5210447e
Jan van Mansum <jan.van.mansum@dans.knaw.nl>
2017-08-24 14:56:35 +0200

    Updated default version

--- generate-easy-module.sh
+++ generate-easy-module.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DEFAULT_ARCHETYPE_VERSION=1.2.0
+DEFAULT_ARCHETYPE_VERSION=1.2.1
 
 read -p "easy-module-archetype version? (default = $DEFAULT_ARCHETYPE_VERSION): " ARCHETYPE_VERSION
 read -p "Module artifactId (e.g., easy-test-module): " ARTIFACT_ID

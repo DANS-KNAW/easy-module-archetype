6b237024093dc099cf200a824cb4a03419dda13a
Richard van Heest <richard.van.heest@dans.knaw.nl>
2018-01-04 22:35:59 +0100

    bump parent version

--- src/main/resources/archetype-resources/src/main/scala/package.scala
+++ src/main/resources/archetype-resources/src/main/scala/package.scala
@@ -1,16 +1,5 @@
 package ${groupId}
 
-import scala.util.{ Failure, Success, Try }
-
 package object ${moduleSubpackage} {
 
-  implicit class TryExtensions2[T](val t: Try[T]) extends AnyVal {
-    // TODO candidate for dans-scala-lib
-    def unsafeGetOrThrow: T = {
-      t match {
-        case Success(value) => value
-        case Failure(throwable) => throw throwable
-      }
-    }
-  }
 }

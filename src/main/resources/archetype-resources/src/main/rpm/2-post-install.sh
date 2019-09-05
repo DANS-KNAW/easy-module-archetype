c27aaa1fc73f38f9a18e8d829507335890e845d6
lindareijnhoudt <linda.reijnhoudt@dans.knaw.nl>
2019-09-05 12:39:24 +0200

    EASY-2235: Set initial heap size during service startup (#44)

--- src/main/resources/archetype-resources/src/main/rpm/2-post-install.sh
+++ src/main/resources/archetype-resources/src/main/rpm/2-post-install.sh
@@ -11,7 +11,8 @@ PHASE="POST-INSTALL"
 
 echo "${symbol_dollar}PHASE: START (Number of current installations: ${symbol_dollar}NUMBER_OF_INSTALLATIONS)"
 service_install_initd_service_script "${symbol_dollar}INSTALL_DIR/install/${symbol_dollar}MODULE_NAME-initd.sh" ${symbol_dollar}MODULE_NAME
-service_install_systemd_unit "${symbol_dollar}INSTALL_DIR/install/${symbol_dollar}MODULE_NAME.service" ${symbol_dollar}MODULE_NAME "${symbol_dollar}INSTALL_DIR/install/override.conf"
+service_install_systemd_unit "${symbol_dollar}INSTALL_DIR/install/${symbol_dollar}MODULE_NAME.service" ${symbol_dollar}MODULE_NAME "${symbol_dollar}INSTALL_DIR/install/charset.conf"
+service_install_systemd_unit "${symbol_dollar}INSTALL_DIR/install/${symbol_dollar}MODULE_NAME.service" ${symbol_dollar}MODULE_NAME "${symbol_dollar}INSTALL_DIR/install/memusage.conf"
 
 service_create_log_directory ${symbol_dollar}MODULE_NAME
 echo "${symbol_dollar}PHASE: DONE"

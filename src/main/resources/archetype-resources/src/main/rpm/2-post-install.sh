644fcfbfd893ea9383149a02b29be28e6f238e7e
Jan van Mansum <jan.van.mansum@dans.knaw.nl>
2019-09-09 10:02:18 +0200

    EASY-1790, EASY-1826, EASY-2264 +journalctl -initd (#45)
    - Upgrade to parent 5.0.0
    - EASY-1790 / EASY-1826 : journalctl-support for services
    - EASY-2264 : remove initd-support from services

--- src/main/resources/archetype-resources/src/main/rpm/2-post-install.sh
+++ src/main/resources/archetype-resources/src/main/rpm/2-post-install.sh
@@ -10,7 +10,6 @@ INSTALL_DIR=/opt/dans.knaw.nl/${symbol_dollar}MODULE_NAME
 PHASE="POST-INSTALL"
 
 echo "${symbol_dollar}PHASE: START (Number of current installations: ${symbol_dollar}NUMBER_OF_INSTALLATIONS)"
-service_install_initd_service_script "${symbol_dollar}INSTALL_DIR/install/${symbol_dollar}MODULE_NAME-initd.sh" ${symbol_dollar}MODULE_NAME
 service_install_systemd_unit "${symbol_dollar}INSTALL_DIR/install/${symbol_dollar}MODULE_NAME.service" ${symbol_dollar}MODULE_NAME "${symbol_dollar}INSTALL_DIR/install/charset.conf"
 service_install_systemd_unit "${symbol_dollar}INSTALL_DIR/install/${symbol_dollar}MODULE_NAME.service" ${symbol_dollar}MODULE_NAME "${symbol_dollar}INSTALL_DIR/install/memusage.conf"
 
